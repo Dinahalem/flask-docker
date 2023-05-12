f5ad320d4324f8c052ea07ad211cd55abb1370ca
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2023-05-12 06:51:05 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-COMMIT_HASH=$(git rev-parse --short HEAD)
+COMMIT_HASH=$(git rev-parse --short HEAD | sed 's/[^a-zA-Z0-9._-]/-/g')
 
 docker build -t dina2022/flaskdocker:${COMMIT_HASH} .
